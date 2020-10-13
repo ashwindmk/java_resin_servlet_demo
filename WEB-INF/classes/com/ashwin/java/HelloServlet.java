78043f585b5e88aea78196c298ba134fb023d4c0
Ashwin <ashwin.dinesh01@gmail.com>
2020-10-13 13:42:08 +0530

    Remove commented code

--- WEB-INF/classes/com/ashwin/java/HelloServlet.java
+++ WEB-INF/classes/com/ashwin/java/HelloServlet.java
@@ -6,10 +6,6 @@ import javax.servlet.http.*;
 import javax.servlet.*;
 
 public class HelloServlet extends HttpServlet {
-    /*public static void main(String... args) {
-        System.out.println("Hello world");
-    }*/
-
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
         out.println("Hello, world!");
